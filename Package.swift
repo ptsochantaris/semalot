a0c5fbea7dd77fec20818ec0602c19f32c9ca32d
Paul Tsochantaris <ptsochantaris@icloud.com>
2023-08-17 16:13:45 +0100

    Bonus ticket fixes

--- Package.swift
+++ Package.swift
@@ -2,61 +2,32 @@
 
 import PackageDescription
 
-#if swift(<5.9)
-
-    let package = Package(
-        name: "Semalot",
-        platforms: [
-            .macOS(.v11),
-            .iOS(.v14),
-            .watchOS(.v7),
-        ],
-        products: [
-            .library(
-                name: "Semalot",
-                targets: ["Semalot"]
-            ),
-        ],
-        dependencies: [
-            .package(url: "https://github.com/ptsochantaris/lista", branch: "main"),
-        ],
-        targets: [
-            .target(
-                name: "Semalot",
-                dependencies: [
-                    .product(name: "Lista", package: "lista"),
-                ]
-            ),
-            .testTarget(
-                name: "SemalotTests",
-                dependencies: ["Semalot"]
-            ),
-        ]
-    )
-
-#else
-
-    let package = Package(
-        name: "Semalot",
-        platforms: [
-            .macOS(.v11),
-            .iOS(.v14),
-            .watchOS(.v7),
-        ],
-        products: [
-            .library(
-                name: "Semalot",
-                targets: ["Semalot"]
-            ),
-        ],
-        targets: [
-            .target(
-                name: "Semalot"),
-            .testTarget(
-                name: "SemalotTests",
-                dependencies: ["Semalot"]
-            ),
-        ]
-    )
-
-#endif
+let package = Package(
+    name: "Semalot",
+    platforms: [
+        .macOS(.v11),
+        .iOS(.v14),
+        .watchOS(.v7),
+    ],
+    products: [
+        .library(
+            name: "Semalot",
+            targets: ["Semalot"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ptsochantaris/lista", branch: "main"),
+    ],
+    targets: [
+        .target(
+            name: "Semalot",
+            dependencies: [
+                .product(name: "Lista", package: "lista"),
+            ]
+        ),
+        .testTarget(
+            name: "SemalotTests",
+            dependencies: ["Semalot"]
+        ),
+    ]
+)
